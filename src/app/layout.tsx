f98baa567362f7ca9d50d67f0f76d0a08c21658c
alsharifm <m7md.alsharif@hotmail.com>
2024-11-22 21:09:12 -0800

    aaa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
-  weight: "100 900",
+  weight: "100 900",  //another test comment
 });
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
