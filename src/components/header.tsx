e66ab5ddc5eab0c0dc3bac52beaf6bd75e1b18c4
rmiranda <rmirandagomez4149@sdsu.edu>
2024-12-03 18:09:33 -0800

    fixed some visual aspects

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import Image from "next/image"
+import Logo from "@/images/InstantReserveLogo.png"
 import { ModeToggle } from '@/components/modetoggle'
 import { Input } from "@/components/ui/input"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
@@ -6,18 +8,20 @@ import { Search, User } from 'lucide-react'
 
 export function Header() {
   return (
-    <header className="flex items-center justify-between px-6 py-4 bg-red-50 border-b border-red-100 static top-0 left-0 right-0">
+    <header className="flex items-center justify-between px-6 py-4 bg-red-600/30 border-b static top-0 left-0 right-0">
       <div className="flex items-center">
-        <button className="text-2xl font-bold text-red-600">
+        <button className="text-3xl font-bold text-red-700">
           <Link href="/">InstantReserve</Link>
         </button>
       </div>
-      <div className="ThemePicker">
+      <div className="flex-1 max-w-md mx-4">
         <ModeToggle />
       </div>
       <div className="flex-1 max-w-md mx-4">
         <div className="relative">
+        <Link href={`/restaurant-results`}>
           <Search className="absolute left-2 top-1/2 h-4 w-4 -translate-y-1/2 text-red-300" />
+        </Link>
           <Input
             type="search"
             placeholder="Search..."
